4bfe1e95a923adde582d156fde014bbc44499569
Penghancurbumi <m.alfakhreza@gmail.com>
2026-09-19 23:59:37 +0700

    refactor remove cuelume sound preview feature and related hooks

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,7 +5,6 @@ import { ThemeProvider } from "next-themes"
 import { TooltipProvider } from "@/components/base/ui/tooltip"
 import { ChatProvider } from "@/components/chat-provider"
 import { WebMCPInitializer } from "@/components/webmcp-initializer"
-import { SoundPreferenceProvider } from "@/hooks/soundcn/use-sound-preference"
 import { LanguagePreferenceProvider } from "@/hooks/use-language-preference"
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -20,12 +19,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
     >
       <TooltipProvider delayDuration={150}>
         <LanguagePreferenceProvider>
-          <SoundPreferenceProvider>
-            <ChatProvider>
-              {children}
-              <WebMCPInitializer />
-            </ChatProvider>
-          </SoundPreferenceProvider>
+          <ChatProvider>
+            {children}
+            <WebMCPInitializer />
+          </ChatProvider>
         </LanguagePreferenceProvider>
       </TooltipProvider>
     </ThemeProvider>
